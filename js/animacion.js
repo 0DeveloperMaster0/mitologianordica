c4ade27fc1425799159584b16d1cfdc088ebabbd
DeveloperMaster0 <gomezangel00000@gmail.com>
2021-09-28 17:36:45 -0400

    primer commit

---
+++ js/animacion.js
@@ -0,0 +1,23 @@
+const mito1 = () => {
+  alert(
+    "Según los mitos nórdicos, antes de que existiera el mundo, no había nada más que un espacio vacío conocido como Ginnungagap. En este vacío, había un mar de fuego y una colección de glaciares. Finalmente, chocaron entre sí. El resultado de la colisión fue Ymir, un ser hermafrodita que creó el primer gigante"
+  );
+};
+
+const mito2 = () => {
+  alert(
+    "Este mito nórdico cuenta una de las historia del dios Loki que un día decidió hacer una broma y pensó que sería extremadamente cómico si afeitara la cabeza de Sif, la esposa de Thor, mientras ella dormía. Después de una cantidad adecuada de ira por parte de Thor, Loki pudo contener la ira física de Thor al prometer un nuevo cabello para Sif que fue creado por enanos."
+  );
+};
+
+const mito3 = () => {
+  alert(
+    "Aunque Thor tuvo varios hijos, se podría argumentar que era más protector con su martillo Mjolnir, que con su descendencia según los mitos nórdicos. De hecho, Thor siguió el camino de guerra cuando se despertó una mañana y descubrió que le habían robado el martillo. El ladrón era un gigante llamado Thrymr."
+  );
+};
+
+const mito4 = () => {
+  alert(
+    "Si había algo que los Aesir odiaban eran los gigantes. El sentimiento era mutuo y se hizo evidente que Asgard estaba bajo una amenaza legítima de los gigantes que comenzaron las peleas sin ninguna razón en particular. Poco después de que se crearon los nueve mundos, el “maestro constructor” sin nombre apareció en Asgard."
+  );
+};
